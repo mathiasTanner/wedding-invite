11deda42bea737ee9875dd83325de6aced3c6311
mathiasTanner <mathias.tanner.ge@gmail.com>
2021-11-21 20:02:44 +0100

    initial commit

---
+++ src/assets/theme/Theme.js
@@ -0,0 +1,32 @@
+import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
+
+const Theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#5a9f5a",
+      main: "#2a702f",
+      dark: "#004404",
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      light: "#ffffff",
+      main: "#e2e2e2",
+      dark: "#b0b0b0",
+      contrastText: "#000000",
+    },
+  },
+  overrides: {
+    MuiButton: {
+      contained: {
+        boxShadow: "none",
+      },
+    },
+    MuiButtonGroup: {
+      contained: {
+        boxShadow: "none",
+      },
+    },
+  },
+});
+
+export default responsiveFontSizes(Theme);
---
+++ src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const useStyles = makeStyles((theme) => ({
+    
+}));
+
+
+const HomePage = (props) => {
+    const classes = useStyles();
+
+    return ();
+}
+
+export default HomePage;
+
+HomePage.propTypes = {};
